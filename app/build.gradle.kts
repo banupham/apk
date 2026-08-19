806d27e0bd261dd5782563460d8e3139b4f1712d
banupham <135731801+banupham@users.noreply.github.com>
2026-08-19 10:09:06 +0700

    Configure Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.banupham.virtualizationtest"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.banupham.virtualizationtest"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
